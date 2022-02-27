8e635d010ef0b9ef23fa0251dd3c01216107778b
Priya <Priyamadiwalar0007@gmail.com>
2022-02-28 00:37:03 +0530

    Added:Welcome to Stacks and Queue

---
+++ src/com/blz/StackandQueueJava/Stack.java
@@ -0,0 +1,7 @@
+package com.blz.StackandQueueJava;
+
+public class Stack {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stacks and Queue");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Day14Assignments {
+}
