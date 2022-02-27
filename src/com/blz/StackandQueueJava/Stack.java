53fe7c1a31c12e181fcb28c2ed40e5d8ae1fe8e8
Priya <Priyamadiwalar0007@gmail.com>
2022-02-28 01:01:07 +0530

    Added:Pop from the Stack

--- src/com/blz/StackandQueueJava/Stack.java
+++ src/com/blz/StackandQueueJava/Stack.java
@@ -40,13 +40,40 @@ public class Stack {
 		top = newNode;
 	}
 
+	// Utility function to check if the stack is empty or not
+	public boolean isEmpty() {
+		return top == null;
+	}
+
+	// Utility function to return top element in a stack
+	public int peek() {
+		// check for empty stack
+		if (!isEmpty()) {
+			return top.data;
+		} else {
+			System.out.println("Stack is empty");
+			return -1;
+		}
+	}
+
+	// Utility function to pop top element from the stack
+	public void pop() { // remove at the beginning
+		// check for stack underflow
+		if (top == null) {
+			System.out.print("\nStack Underflow");
+			return;
+		}
+
+		// update the top pointer to point to the next node
+		top = (top).next;
+	}
+
 	public void display() { // print method
 		Node temp = top;
 		while (temp != null) {
 			System.out.print(temp.data + "-> ");
 			temp = temp.next;
 		}
-
 	}
 
 	public static void main(String[] args) {
@@ -54,11 +81,27 @@ public class Stack {
 		System.out.println("Welcome to Stack and Queue Problem....!!!");
 		Stack stack = new Stack();
 
+		System.out.println();
 		System.out.println("Element present in the Stack");
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
 		stack.display();
-	}
 
+		// print Top element of Stack
+		System.out.println();
+		stack.pop();
+		stack.display();
+		System.out.printf("\nTop element is %d\n", stack.peek());
+
+		System.out.println();
+		stack.pop();
+		stack.display();
+		System.out.printf("\nTop element is %d\n", stack.peek());
+
+		System.out.println();
+		stack.pop();
+		stack.display();
+		System.out.printf("\nTop element is %d\n", stack.peek());
+	}
 }
